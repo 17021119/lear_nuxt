864c59331453c8046692806bf702fd5dde1331cd
TuNguyen <ituet1119@gmail.com>
2021-12-22 21:34:01 +0700

    28.2 get data from api set into store nuxtServerInit()

--- store/index.js
+++ store/index.js
@@ -12,6 +12,32 @@ const createStore = () => {
             }
         },
         actions: {
+            nuxtServerInit(vuexContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit('setDecks', [{
+                                _id: 1,
+                                name: "Learn English",
+                                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                thumbnail: "https://wallpaperaccess.com/full/6617143.png"
+                            },
+                            {
+                                _id: 2,
+                                name: "Learn Chinese",
+                                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                thumbnail: "https://wallpaperaccess.com/full/6617143.png"
+                            },
+                            {
+                                _id: 3,
+                                name: "Learn Japanese",
+                                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                                thumbnail: "https://wallpaperaccess.com/full/6617143.png"
+                            }
+                        ])
+                        resolve(); // muốn thực thi
+                    }, 1500)
+                })
+            },
             setDecks(vuexContext, decks) {
                 vuexContext.commit('setDecks', decks);
             }
